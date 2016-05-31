8fad3d3af7707dc3cd9dd374159e0ade57dc1a54
mmanik726 <mmanik@asu.edu>
2016-05-31 15:51:37 -0700

    minor changes

--- CSE360Projects/src/cse360assign2/Analytics.java
+++ CSE360Projects/src/cse360assign2/Analytics.java
@@ -1,11 +1,18 @@
 package cse360assign2;
 
+/**
+ * extended OrderedIntList. 
+ * @author Mohammed Manik (pin: 34)
+ * @version 1.0
+ */
 public class Analytics extends OrderedIntList{
-
-	
+    	/**
+     	*
+     	* This method gets the mean of the elements in list
+     	* @return returns -1 if list is empty, else returns the mean of the elements in list
+     	*/
 	public float mean()
 	{
-		
 		if(count == 0)
 		{
 			return -1;
@@ -13,22 +20,22 @@ public class Analytics extends OrderedIntList{
 		else
 		{
 			//float totalCount = intArray.length;
-			
 			float mean = 0;
 			float sum = 0; 
-			
 			for (int i = 0; i < count; i++)
 			{
 				sum = sum + intArray[i];
 			}
-			
 			mean = sum / count;
-			
 			return mean;
-				
 		}
 	}
 	
+    	/**
+     	*
+     	* This method gets the median of the elements in list
+     	* @return returns -1 if list is empty, else returns the median of the elements in list
+     	*/
 	public int median()
 	{
 		if(count == 0)
@@ -41,6 +48,11 @@ public class Analytics extends OrderedIntList{
 		}
 	}
 	
+    	/**
+     	*
+     	* This method gets the highest elements in list
+     	* @return returns -1 if list is empty, else returns the highest elements in list
+     	*/
 	public int high()
 	{
 		if(count == 0)
@@ -52,6 +64,12 @@ public class Analytics extends OrderedIntList{
 			return intArray[count-1];				
 		}
 	}
+
+    	/**
+     	*
+     	* This method gets the lowest elements in list
+     	* @return returns -1 if list is empty, else returns the lowest elements in list
+     	*/
 	public int low()
 	{
 		if(count == 0)
@@ -63,6 +81,12 @@ public class Analytics extends OrderedIntList{
 			return intArray[0];				
 		}
 	}
+
+    	/**
+     	*
+     	* This method gets the number of elements in list
+     	* @return returns -1 if list is empty, else returns the number of elements in list
+     	*/
 	public int numInts()
 	{
 		if(count == 0)
