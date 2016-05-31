5a026bb46e056554437728317a7792762fae2cfc
mmanik726 <mmanik@asu.edu>
2016-05-31 15:49:28 -0700

    Update TestAnalytics.java

--- CSE360Projects/src/cse360assign2/TestAnalytics.java
+++ CSE360Projects/src/cse360assign2/TestAnalytics.java
@@ -1,7 +1,14 @@
 package cse360assign2;
-
+/**
+ * Analytics testing class. 
+ * @author Mohammed Manik (pin: 34)
+ * @version 1.0
+ */
 public class TestAnalytics {
 
+   	/**
+    	*driver class for the analytics class.
+     	*/
 	public static void main()
 	{
         Analytics myList = new Analytics();
@@ -12,11 +19,20 @@ public class TestAnalytics {
         
         myList.print();
         
-        
+
+	System.out.println("mean:");        
         System.out.println(myList.mean());
+
+	System.out.println("median:");
         System.out.println(myList.median());
+
+	System.out.println("highest:");
         System.out.println(myList.high());
+
+	System.out.println("lowest:");
         System.out.println(myList.low());
+
+	System.out.println("number of integers:");
         System.out.println(myList.numInts());
 		
 	}
