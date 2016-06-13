436784ef7387fb6f85d6c4fdb19f525e3d8e7491
Mohammed Manik <mmanik@asu.edu>
2016-06-13 00:02:15 -0700

    Added logic to assign3 class and test class

--- CSE360Projects/src/cse360assign3/Assign3.java
+++ CSE360Projects/src/cse360assign3/Assign3.java
@@ -1,5 +1,42 @@
 package cse360assign3;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Assign3 {
 
+	
+	public void readInt()
+	{
+		Scanner scanner = new Scanner(System.in);
+		
+		
+		ArrayList<Integer> myInts = new ArrayList<>(); //=  new ArrayList<>();
+
+		int myNums[] = new int[5];
+		
+		for (int i = 0; i< 5; i++)
+		{
+			System.out.println("Please enter an intger");
+			int inputNum = scanner.nextInt();
+			myNums[i] = inputNum;
+			
+		}
+		
+
+		System.out.println("You have entered: ");
+		for (int i = 0; i< 5; i++)
+		{
+
+			System.out.println(myNums[i]);
+			
+		}
+		
+		//System.out.println(" Hello");
+		
+		scanner.close();
+		
+	}
+	
+	
 }
--- CSE360Projects/src/cse360assign3/TestAssign3.java
+++ CSE360Projects/src/cse360assign3/TestAssign3.java
@@ -4,7 +4,14 @@ public class TestAssign3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+	
+		Assign3 testing =  new Assign3();
+		
+		testing.readInt();
+		
+		
+		
+		
 	}
 
 }
