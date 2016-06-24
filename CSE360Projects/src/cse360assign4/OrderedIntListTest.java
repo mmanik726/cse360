36c9de8b817e7b0841d7b299f118ccffc5d16cc6
mmanikCK <mmanik@CK.com>
2016-06-24 11:45:28 -0700

    Added junit test class for OrderedIntList class with empty methods

---
+++ CSE360Projects/src/cse360assign4/OrderedIntListTest.java
@@ -0,0 +1,39 @@
+package cse360assign4;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class OrderedIntListTest {
+
+	@Test
+	public void testOrderedIntList() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testOrderedIntListInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSearch() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDelete() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInsert() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
